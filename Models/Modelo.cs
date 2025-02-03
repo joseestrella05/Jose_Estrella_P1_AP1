c3e719685cb88a51e91ef4bc866363b58bcc43da
Jose Gabriel <Jose Gabriel@DESKTOP-SQIUKTF>
2025-02-03 18:38:52 -0400

    Modelo update

--- Models/Modelo.cs
+++ Models/Modelo.cs
@@ -1,6 +1,10 @@
-﻿namespace Jose_Estrella_P1_AP1.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Jose_Estrella_P1_AP1.Models;
 
 public class Modelo
 {
+    [Key]
+    public int Id { get; set; }
 
 }
